c51b061dab5e91bc61faa614fa743dbf8cec06ed
Cenk Gündoğan <mail-github@cgundogan.de>
2018-04-19 12:48:39 +0200

    ccn-lite: check for pkt before use

--- src/ccnl-riot/src/ccn-lite-riot.c
+++ src/ccnl-riot/src/ccn-lite-riot.c
@@ -606,6 +606,8 @@ ccnl_send_interest(struct ccnl_prefix_s *prefix, unsigned char *buf, int buf_len
         return -4;
     }
 
+    pkt->to = to;
+
     msg_t m = { .type = GNRC_NETAPI_MSG_TYPE_SND, .content.ptr = pkt };
     ret = msg_send(&m, ccnl_event_loop_pid);
     if(ret < 1){
