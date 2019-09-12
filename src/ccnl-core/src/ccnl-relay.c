df377b3dc1ee5a99b8df8ff626f1624ee9e843b1
Cenk Gündoğan <mail+dev@gundogan.net>
2019-09-12 13:35:54 +0200

    fix cs handling

--- src/ccnl-core/src/ccnl-relay.c
+++ src/ccnl-core/src/ccnl-relay.c
@@ -645,7 +645,13 @@ ccnl_content_add2cache(struct ccnl_relay_s *ccnl, struct ccnl_content_s *c)
     }
 
     if (ccnl->max_cache_entries > 0 && ccnl->contentcnt >= ccnl->max_cache_entries) {
-        cache_strategy_remove(ccnl, c);
+        if (cache_strategy_remove(ccnl, c) == 0) {
+            if (c->pkt) {
+                ccnl_pkt_free(c->pkt);
+            }
+            ccnl_free(c);
+            return NULL;
+        }
     }
 
     if ((ccnl->max_cache_entries <= 0) || (ccnl->contentcnt < ccnl->max_cache_entries)) {
