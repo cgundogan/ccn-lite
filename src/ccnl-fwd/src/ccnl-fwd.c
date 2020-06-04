f0ace3aef7297675c60877df8f1c6dda7149be04
Cenk Gündoğan <mail+dev@gundogan.net>
2020-06-05 01:38:00 +0200

    irx

--- src/ccnl-fwd/src/ccnl-fwd.c
+++ src/ccnl-fwd/src/ccnl-fwd.c
@@ -217,6 +217,10 @@ ccnl_fwd_handleInterest(struct ccnl_relay_s *relay, struct ccnl_face_s *from,
 #endif
     }
 
+    ccnl_prefix_to_str((*pkt)->pfx, s, CCNL_MAX_PREFIX_SIZE);
+
+    printf("irx;%.*s\n", 5, s+19);
+
 #ifdef USE_DUP_CHECK
 
     if (ccnl_nonce_isDup(relay, *pkt)) {
@@ -250,8 +254,6 @@ ccnl_fwd_handleInterest(struct ccnl_relay_s *relay, struct ccnl_face_s *from,
 
             // Step 1: search in content store
     DEBUGMSG_CFWD(DEBUG, "  searching in CS\n");
-
-    ccnl_prefix_to_str((*pkt)->pfx, s, CCNL_MAX_PREFIX_SIZE);
 
     for (c = relay->contents; c; c = c->next) {
         if (c->pkt->pfx->suite != (*pkt)->pfx->suite)
